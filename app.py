7155df271d102bc5a09d439052aa5891175deb18
Trevor Powell <tjpowell@usc.edu>
2025-08-17 16:44:49 -0500

    Wrong port for render

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def predict_datapoint():
 
 if __name__=="__main__":
     import os
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
